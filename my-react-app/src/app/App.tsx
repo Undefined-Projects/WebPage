97b6e4de91f775b67fabd3d01301e20f2940490d
AlanOlea06 <alanoleabatres@gmail.com>
2026-04-07 17:32:31 -0600

    Add Novedades.css for styling Novedades component layout and design

--- my-react-app/src/app/App.tsx
+++ my-react-app/src/app/App.tsx
@@ -1,4 +1,5 @@
 import Title from './Title.tsx'
+import Novedades from '../components/Novedades.tsx'
 
 function App() {
   
@@ -6,7 +7,7 @@ function App() {
   return (
     <>
       <Title/>
-
+      <Novedades/>
     </>
   )
 }
---
+++ my-react-app/src/components/Novedades.tsx
@@ -0,0 +1,19 @@
+import './Novedades.css'
+
+function Novedades() {
+    return (
+        <>
+            <div className='content'>
+                <div className='cuadro'>
+                    <div className='imagen'></div>
+                    <div className='texto'>
+                        <h2>¡Novedades!</h2>
+                        <p>¡Estamos emocionados de anunciar que hemos lanzado una nueva función en nuestra aplicación! Ahora puedes personalizar tu perfil con una variedad de temas y colores. ¡Haz que tu experiencia sea única y refleja tu estilo personal! Explora las opciones de personalización en la configuración de tu cuenta y dale un toque especial a tu perfil. ¡Esperamos que disfrutes esta nueva función tanto como nosotros disfrutamos desarrollándola para ti!</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Novedades
